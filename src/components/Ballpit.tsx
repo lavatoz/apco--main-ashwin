b5817ade4177f1e2445cdb60cdb86ca1e2802a4a
joel abin <joelabin854796@gmail.com>
2026-07-25 21:16:11 +0530

    feat: implement Ballpit component with custom Three.js engine and pointer interaction handler

--- src/components/Ballpit.tsx
+++ src/components/Ballpit.tsx
@@ -256,8 +256,8 @@ function S(e) {
         document.body.addEventListener('pointerleave', L);
         document.body.addEventListener('click', C);
 
-        document.body.addEventListener('touchstart', TouchStart, { passive: false });
-        document.body.addEventListener('touchmove', TouchMove, { passive: false });
+        document.body.addEventListener('touchstart', TouchStart, { passive: true });
+        document.body.addEventListener('touchmove', TouchMove, { passive: true });
         document.body.addEventListener('touchend', TouchEnd, { passive: false });
         document.body.addEventListener('touchcancel', TouchEnd, { passive: false });
 
@@ -328,7 +328,6 @@ function L() {
 
 function TouchStart(e) {
   if (e.touches.length > 0) {
-    e.preventDefault();
     A.x = e.touches[0].clientX;
     A.y = e.touches[0].clientY;
 
@@ -349,7 +348,6 @@ function TouchStart(e) {
 
 function TouchMove(e) {
   if (e.touches.length > 0) {
-    e.preventDefault();
     A.x = e.touches[0].clientX;
     A.y = e.touches[0].clientY;
 
